56d6502324a650d53c3b9cd91ad513a9734d0e42
pawel.zoladek <pawel.zoladek@intive.com>
2020-07-03 15:10:58 +0200

    Add price

--- src/main/java/wizut/tpsi/ogloszenia/OgloszeniaApplication.java
+++ src/main/java/wizut/tpsi/ogloszenia/OgloszeniaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OgloszeniaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("ch1");
 		SpringApplication.run(OgloszeniaApplication.class, args);
 	}
 
